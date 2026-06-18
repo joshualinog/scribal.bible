584d8cd3081f10e44431b2c15991331bd8159c7a
Joshua Andrew Linog <jsinkcreations@gmail.com>
2026-06-18 17:55:39 +0000

    chore: update navigation and latest-posts layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
   content: ["./src/**/*.{html,njk,js}"],
+  safelist: [
+    {
+      pattern: /bg-(cream|brick-red|slate-blue|olive-gold|forest-green|ochre|navy|mustard|tan|muted-blue|dark-black|light-gray|medium-gray|charcoal|off-black)-\d{2,3}/,
+    },
+    {
+      pattern: /text-(cream|brick-red|slate-blue|olive-gold|forest-green|ochre|navy|mustard|tan|muted-blue|dark-black|light-gray|medium-gray|charcoal|off-black)-\d{2,3}/,
+    },
+  ],
   theme: {
     extend: {
+      screens: {
+        xs: '320px',
+        ml: '960px',
+      },
       fontFamily: {
-        sans: ["Rubik", "ui-sans-serif", "system-ui", "sans-serif"],
+        sans: ["Barlow Condensed", "ui-sans-serif", "system-ui", "sans-serif"],
+        heading: ["UnifrakturCook", "serif"],
+        display: ["Manufacturing Consent", "serif"],
+        accent: ["Pirata One", "serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
